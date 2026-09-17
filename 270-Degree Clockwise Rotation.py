bbd438a8b40d1cfc58ff4832fe2e4601a90377ed
prashanthnaik-1803 <ramavathprashanth11@gmail.com>
2026-09-17 16:20:59 +0530

    Change image file and rotate to 270 degrees
    Updated image file name and modified rotation to 270 degrees clockwise.

---
+++ 270-Degree Clockwise Rotation.py
@@ -0,0 +1,16 @@
+import cv2
+
+image = cv2.imread("image.jpg")
+
+rotated_image = cv2.rotate(
+    image,
+    cv2.ROTATE_90_COUNTERCLOCKWISE
+)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("270 Degree Clockwise", rotated_image)
+
+cv2.imwrite("rotated_image_270.jpg", rotated_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
